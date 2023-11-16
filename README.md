2155f6be6974b1c1110110c2e11a52ce97cb4678
jcollier <jcollier@scottlogic.com>
2023-11-16 16:02:25 +0000

    Add action to schedule jobs

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Carbon Aware Scheduler
-This action is to be used to schedule jobs when the carbon intesity of a chosen region is low using the carbon aware API
+The Carbon Aware Scheduler is a GitHub Action that can be used to schedule GitHub workflows to run at low carbon intesity periods, making use of the Carbon Aware WebApi 
 
 ## Usage
-This action can be included as part of an initial set up job, to then schedule a larger job to run when the carbon intesity is low.
+The GitHub Action can be used in an initial setup workflow to schedule other workflows to run at the lowest carbon intense time in the next 24hrs. This makes use of a templating structure, where the workflows that are to be scheduled are added into a template folder, where they can be automatically scheduled with the output of the action.  
+
+### Example usage
+
+```yaml
+  - uses: jhcoll/carbon-aware-schedule-action@v1
+    with:
+        # Region to check carbon intensity for - can be any azure region - however if using github hosted runners will be one of the following - 'eastus, eastus2, westus2, centralus, southcentralus'
+        region: 'eastus'
+        # path to template job to be scheduled
+        template-paths: ['templates/template.yml'] 
+        # Time to check until - In format YYYY-MM-DDThh:mm:ssZ
+        end-time: '2023-11-16T11:00:00Z' # Optional - defaults to +24hr
+        # Estimated job length in minutes
+        estimated-job-length: '10' # Optional - defaults to 10
+        # Output path for scheduled jobs
+        output-path: '.github/workflows/' # Optional - defaults to ".github/workflows/"
+```
---
+++ index.js
@@ -0,0 +1,81 @@
+const core = require('@actions/core');
+const fs = require("fs");
+
+const REGION = core.getInput('region') != '' ? core.getInput('region') : 'eastus';
+const REPO_DIRECTORY = process.env.GITHUB_WORKSPACE != undefined ? process.env.GITHUB_WORKSPACE : '.';
+const TEMPLATE_PATHS = core.getInput('template-paths') != '' ? core.getInput('template-paths') : [`templates/template.yml`];
+const OUTPUT_PATH = core.getInput('output-path') != '' ? core.getInput('output-path') : [`.github/workflows/`];
+const JOB_LENGTH = core.getInput('estimated-job-length') != '' ? core.getInput('estimated-job-length') : 10;
+
+(async () => {
+  try {
+    endTime = core.getInput('end-time');
+    startTime = new Date();
+
+    if(endTime == '') {
+      endTime = new Date()
+      endTime.setDate(startTime.getDate() + 1);
+    }
+
+    searchParams = new URLSearchParams({
+      location: REGION,
+      dateStartAt: startTime.toISOString().replace(/\.[0-9]{3}/, ''),
+      dataEndAt: endTime.toISOString().replace(/\.[0-9]{3}/, ''),
+      windowSize: JOB_LENGTH
+    })
+
+    core.debug('Search Params Used: ' + searchParams);
+
+    data = await fetch("https://carbon-aware-api.azurewebsites.net/emissions/forecasts/current?" + searchParams,
+    {
+      method: "GET",
+      headers: {"Content-Type": "application/json"},
+    })
+    .then((res) => {
+      if(res.status !== 200) {
+        console.log(res);
+        throw Error(res.statusText);
+      }
+      return res.json()
+    });
+
+    timestamp = new Date(data[0].optimalDataPoints[0].timestamp);
+    core.debug('Workflow to be scheduled at: ' + timestamp);
+
+    cron = dateToCron(timestamp);
+    updatedFiles = await writeSchedule(cron);
+
+    core.debug('Jobs to be scheduled: ' + updatedFiles);
+    core.setOutput("schedules-workflows", updatedFiles);
+    core.setOutput("scheduled-time", timestamp);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
+
+const dateToCron = (date) => {
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const days = date.getDate();
+  const months = date.getMonth() + 1;
+  const dayOfWeek = date.getDay();
+
+  return `${minutes} ${hours} ${days} ${months} ${dayOfWeek}`;
+};
+
+const writeSchedule = async (cron) => {
+  updatedFiles = [];
+  for (filePath of TEMPLATE_PATHS) {
+    try{
+      const filename = filePath.split('/').pop();
+      const data = await fs.readFileSync(`${REPO_DIRECTORY}/${filePath}`, 'utf8');
+      const result = data.replace(/\$schedule\$/g, cron);
+      await fs.writeFileSync(`${REPO_DIRECTORY}/${OUTPUT_PATH}/${filename}`, result, 'utf8');
+      updatedFiles.push(`${OUTPUT_PATH}/${filename}`);
+    } catch(err) {
+      core.warning(`Could not schedule ${filePath}`);
+    }
+  }
+  return updatedFiles;
+};
